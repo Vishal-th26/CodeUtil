6e856f1b7e43029ad938fac49847df192c8953e0
Vishal Thakur <Vishalth023@gmail.com>
2026-07-13 15:47:00 +0530

    ultra final ngl bro

--- backend/embeddings/embedder.py
+++ backend/embeddings/embedder.py
@@ -1,11 +1,12 @@
 
 
-
+import torch
 from langchain_community.embeddings import HuggingFaceEmbeddings
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5",
-    model_kwargs={"device": "cuda"},
-    encode_kwargs={"normalize_embeddings": True}
+    model_kwargs={"device": device},
+    encode_kwargs={"normalize_embeddings": True},
 )
-
